99f29293ef5d80451b9629d6bc354828c356f5bf
zzh <1760655799@qq.com>
2020-09-20 21:39:32 +0800

    feat(middleware): 中间件

--- dist/common/middlewares/logger.js
+++ dist/common/middlewares/logger.js
@@ -6,23 +6,17 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.AppModule = void 0;
+exports.LoggerMiddle = void 0;
 const common_1 = require("@nestjs/common");
-const app_controller_1 = require("./app.controller");
-const app_service_1 = require("./app.service");
-const hello_module_1 = require("./modules/hello/hello.module");
-const logger_middleware_1 = require("./common/middlewares/logger.middleware");
-let AppModule = class AppModule {
-    configure(consumer) {
-        consumer.apply(logger_middleware_1.LoggerMiddleware).forRoutes("hello");
+let LoggerMiddle = class LoggerMiddle {
+    use(req, res, next) {
+        const { method, path } = req;
+        console.log(`${method} ${path}`);
+        next();
     }
 };
-AppModule = __decorate([
-    common_1.Module({
-        imports: [hello_module_1.HelloModule],
-        controllers: [app_controller_1.AppController],
-        providers: [app_service_1.AppService],
-    })
-], AppModule);
-exports.AppModule = AppModule;
-//# sourceMappingURL=app.module.js.map
+LoggerMiddle = __decorate([
+    common_1.Injectable()
+], LoggerMiddle);
+exports.LoggerMiddle = LoggerMiddle;
+//# sourceMappingURL=logger.js.map
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,15 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import {HelloModule} from './modules/hello/hello.module';
-
+import { Module, NestModule, MiddlewareConsumer } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { HelloModule } from "./modules/hello/hello.module";
+import { LoggerMiddleware } from "./common/middlewares/logger.middleware";
 @Module({
-  imports: [HelloModule],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [HelloModule],
+    controllers: [AppController],
+    providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+    configure(consumer: MiddlewareConsumer) {
+        consumer.apply(LoggerMiddleware).forRoutes("hello");
+    }
+}
